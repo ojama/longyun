edcc743f2dbbd30f65fa325fd8700c3b99ae2c75
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-03-20 00:40:43 +0800

    初始化

--- mysql.php
+++ mysql.php
@@ -1,11 +1,11 @@
 <?php
 return array (
   'DB_TYPE' => 'mysql',
-  'DB_HOST' => 'localhost',
-  'DB_NAME' => 'lanke',
-  'DB_USER' => 'r',
-  'DB_PWD' => 'r',
-  'DB_PORT' => '3306',
+  'DB_HOST' => '55a32a9887e03.gz.cdb.myqcloud.com',
+  'DB_NAME' => 'longyun',
+  'DB_USER' => 'cdb_outerroot',
+  'DB_PWD' => 'Libo1234',
+  'DB_PORT' => '16273',
   'DB_PREFIX' => 'lanke_',
 );
 ?>
